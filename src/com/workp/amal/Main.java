fae3ba2d978a7237f3f186428973db7f16cba281
amal-ess <essoubki.amal@gmail.com>
2019-01-10 15:19:59 +0100

    ajout de nouveaux produits et informations supplémentaire (extends)

--- src/com/workp/amal/Main.java
+++ src/com/workp/amal/Main.java
@@ -1,8 +1,13 @@
 package com.workp.amal;
 
+import com.workp.amal.garage.Car;
+import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	Car car = new Car ("Clio III",
+            "Renault Clio 3 Gris année 2005", "Renault", 2005, "Gris",
+    225, new int[]{4832, 1720, 1497}, 1240, 5, 5, "motor name",
+            List.of(new String[]{"vitres éléctriques", "GPS"}));
     }
 }
---
+++ src/com/workp/amal/garage/Car.java
@@ -0,0 +1,44 @@
+package com.workp.amal.garage;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Car extends Vehicle {
+    private int door;
+    private int litrePer100km;
+    private String motor;
+    private List<String> options = new ArrayList<String>();
+
+    public Car(String modelName, String description, String manufacturer, int year, String color, int speed, int[] dimensions, int weight, int door, int litrePer100km, String motor, List<String> options) {
+        super(modelName, description, manufacturer, year, color, speed, dimensions, weight);
+        this.door = door;
+        this.litrePer100km = litrePer100km;
+        this.motor = motor;
+        this.options = options;
+    }
+
+
+    public void startHeadlight(){
+        System.out.println("J'allume mes phares");
+    }
+
+    public void openTrunk(){
+        System.out.println("J'ouvre mon coffre");
+    }
+
+    public int getDoor() {
+        return door;
+    }
+
+    public int getLitrePer100km() {
+        return litrePer100km;
+    }
+
+    public String getMotor() {
+        return motor;
+    }
+
+    public List<String> getOptions() {
+        return options;
+    }
+}
